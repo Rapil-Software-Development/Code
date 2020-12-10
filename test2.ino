44cdf7e47a0cccb15418a3de414ba33b38b46665
IK-31-zdebeliak <55227583+IK-31-zdebeliak@users.noreply.github.com>
2020-12-10 14:29:57 +0200

    Add files via upload

---
+++ test2.ino
@@ -0,0 +1,125 @@
+#include <WiFi.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define WIFI_SSID "434a"
+#define WIFI_PASSWORD "qwerty12345"
+
+
+#define MQTT_HOST "broker.emqx.io"
+#define MQTT_PORT 1883
+
+// Temperature MQTT Topic
+#define MQTT_PUB_TEMP "esp32/ds18b20/temperature"
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 15;          
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+// Temperature value
+float temp;
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 5000;        // Interval at which to publish sensor readings
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  // Start the DS18B20 sensor
+  sensors.begin();
+  
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+   //mqttClient.setCredentials("USER", "PASSWORD");
+  connectToWifi();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    // New temperature readings
+    sensors.requestTemperatures(); 
+    // Temperature in Celsius degrees
+    temp = sensors.getTempCByIndex(0);
+    // Temperature in Fahrenheit degrees
+    //temp = sensors.getTempFByIndex(0);
+    
+    // Publish an MQTT message on topic esp32/ds18b20/temperature
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: ", MQTT_PUB_TEMP);
+    Serial.println(packetIdPub1);
+    Serial.printf("Message: %.2f /n", sensors.getTempCByIndex(0));
+  }
+}
